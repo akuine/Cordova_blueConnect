bd002b4fb08fe455802584f48abb70d21ac705b0
akuine <2585037007@qq.com>
2024-12-10 19:27:48 +0800

    包名称修改

--- hxtBdPlugin/scripts/append_build_extras.gradle.js
+++ hxtBdPlugin/scripts/append_build_extras.gradle.js
@@ -1,18 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 module.exports = function (context) {
-    const platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-    const buildGradlePath = path.join(platformRoot, 'app/build.gradle');
+  const platformRoot = path.join(context.opts.projectRoot, "platforms/android");
+  const buildGradlePath = path.join(platformRoot, "app/build.gradle");
 
-    if (fs.existsSync(buildGradlePath)) {
-        const content = fs.readFileSync(buildGradlePath, 'utf-8');
-        if (!content.includes("implementation(name: 'blue-releasev_20241125', ext: 'aar')")) {
-            fs.appendFileSync(buildGradlePath, `
+  if (fs.existsSync(buildGradlePath)) {
+    const content = fs.readFileSync(buildGradlePath, "utf-8");
+    if (
+      !content.includes(
+        "implementation(name: 'blue-releasev_20241125', ext: 'aar')"
+      )
+    ) {
+      fs.appendFileSync(
+        buildGradlePath,
+        `
 dependencies {
    implementation(name: 'blue-releasev_20241125', ext: 'aar')
 }
-`);
-        }
+`
+      );
     }
+  }
 };
